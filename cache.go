34319a5a996d595bfdf6ea75fadf3f9d13d99ed1
Yauheni Ramankou <iromankov2010@gmail.com>
2022-06-01 20:22:11 +0300

    feat: implement cache task without expiring keys

--- cache.go
+++ cache.go
@@ -1,23 +1,44 @@
 package cache
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Cache struct {
+	kvPairs map[string]string
+	keys    []string
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (c Cache) Get(key string) (string, bool) {
+	fmt.Println(c)
+	_, ok := c.kvPairs[key]
+	if !ok {
+		return "", false
+	}
+	return c.kvPairs[key], true
 }
 
-func (receiver) Put(key, value string) {
+func (c *Cache) Put(key, value string) {
+	c.keys = append(c.keys, key)
+	if c.kvPairs == nil {
+		c.kvPairs = make(map[string]string)
+		c.kvPairs[key] = value
+	} else {
+		c.kvPairs[key] = value
+	}
+
+	fmt.Println("Here we putting the value")
 }
 
-func (receiver) Keys() []string {
+func (c Cache) Keys() []string {
+	return c.keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (c Cache) PutTill(key, value string, deadline time.Time) {
+	fmt.Println(deadline)
 }
